2c5037ddc0f25d8be27d7927d2603e6ea4766ff5
smenzel18 <szymonmenzel18@gmail.com>
2022-03-17 17:23:01 +0100

    Save depth images

--- scripts/image_saver_server.py
+++ scripts/image_saver_server.py
@@ -6,15 +6,25 @@ import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from image_generator.srv import ImageToSave, ImageToSaveResponse
-
+import numpy as np
 
 def handle_image_to_save(req):
     bridge = CvBridge()
-    try:
-        cv2_img = bridge.imgmsg_to_cv2(req.image, desired_encoding='passthrough')
-    except CvBridgeError as e:
-        print(e)
-        return ImageToSaveResponse(False)
+    if 'depth' in req.filename:
+        try:
+            req.image.encoding = "32FC1"
+            cv2_img = bridge.imgmsg_to_cv2(req.image, desired_encoding='passthrough')
+            cv2_img = cv2_img * 63
+        except CvBridgeError as e:
+            print(e)
+            return ImageToSaveResponse(False)
+    elif 'rgb' in req.filename:
+        try:
+            req.image.encoding = "bgr8"
+            cv2_img = bridge.imgmsg_to_cv2(req.image, desired_encoding='passthrough')
+        except CvBridgeError as e:
+            print(e)
+            return ImageToSaveResponse(False)
     os.makedirs(os.path.abspath("./src/image_generator/images"), exist_ok=True)
     cv2.imwrite(os.path.abspath(f"./src/image_generator/images/{req.index}_{req.filename}.png"), cv2_img)
     return ImageToSaveResponse(True)
@@ -31,3 +41,4 @@ if __name__ == "__main__":
         image_saver_server()
     except rospy.ROSInterruptException:
         pass
+#dataStencil
